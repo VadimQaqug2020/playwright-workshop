a0179b86f28c4aa32e1d986bdc6452fb3475666e
v.savchenko <v.savchenko@polytech.software>
2023-07-29 11:48:09 +0300

    rm headed

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,8 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
-    headless: false, },
+      headless: false, 
+      },
     },
 
     // {
